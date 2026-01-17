aca1ac2f9a3dcbbe70e9fe63752f553bc66e89cd
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2026-01-18 03:10:30 +0530

    Create hospitalAuth.js

---
+++ backend/routes/hospital/hospitalAuth.js
@@ -0,0 +1,95 @@
+import jwt from 'jsonwebtoken';
+import express from 'express';
+import bcrypt from 'bcrypt';
+import { Hospital } from '../../models/Hospital.js';
+
+const router = express.Router();
+
+// hospital login
+router.post('/api/hospital/login', async (req, res) => {
+  const { email, password } = req.body;
+
+    try{
+        const present = await Hospital.findOne({email});
+
+        if(!present){
+            return res.status(404).json({
+                success: false,
+                message: "User not found",
+            });
+        }
+
+        const isMatch = await bcrypt.compare(password, present.password);
+
+        if(!isMatch){
+            return res.status(400).json({
+                success: false,
+                message: "Invalid password",
+            });
+        }
+
+        const token = jwt.sign({email: email, name: present.name, userId: present._id, role: 'HOSPITAL' }, process.env.JWT_SECRET, {expiresIn: '1d'});
+
+        res.cookie('token', token, {
+          httpOnly: true,
+          maxAge: 840000,
+          secure: false,
+          sameSite: 'strict'
+        });
+      
+        return res.status(200).json({
+            success: true,
+            message: "Login seccessfull",
+            token
+        });
+
+    } 
+    catch(err){
+        console.log(err.message);
+        return res.status(500).json({
+            success: false,
+            message: "Something went wrong",
+        });
+    }
+});
+
+// hospital register
+router.post('/api/hospital/register', async (req, res) => {
+  const { name, email, password, hospital } = req.body;
+ 
+    try{
+        const present = await Hospital.findOne({email});
+        
+        if(present){
+            return res.status(400).json({
+                success: false,
+                message: "User already exist",
+            });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newHospital = new Hospital({
+            name, 
+            email, 
+            password: hashedPassword,
+            hospital
+        });
+
+        await newHospital.save();
+
+        return res.status(200).json({
+            success: true,
+            message: "User created successfull",
+        });
+
+    }
+    catch(err){
+        return res.status(500).json({
+            success: false,
+            message: "Something went wrong",
+        });
+    }
+});
+
+export default router:
