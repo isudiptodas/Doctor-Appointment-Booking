71db4acc998cb0756db0fbf6f23237253c94adfb
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2026-01-18 04:24:40 +0530

    Update authenticate.js

--- backend/middleware/authenticate.js
+++ backend/middleware/authenticate.js
@@ -11,10 +11,25 @@ export const authenticate = async (req, res, next) => {
     });
   }
 
+  const roleRoutes = {
+    'USER' : '/api/user',
+    'DOCTOR' : '/api/doctor',
+    'HOSPITAL' : '/api/hospital',
+  };
+
   try{
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const role = decoded.role;
     req.userData = decoded;
-    next();
+
+    if(req.path.startsWith(roleRoutes[role])){
+      next();
+    }
+    
+    return res.status(403).json({
+      success: false,
+      message: 'Unauthorized'
+    });
   }
   catch(err){
     console.log(err);
