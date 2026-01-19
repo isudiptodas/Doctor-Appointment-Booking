331e774382dd68243ee3f3c296e59ab8ca31648a
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2026-01-19 19:16:37 +0530

    Create arcjetSecurity.js

---
+++ backend/config/arcjetSecurity.js
@@ -0,0 +1,22 @@
+import arcjet, { shield, detectBot, tokenBucket } from "@arcjet/node";
+import { isSpoofedBot } from "@arcjet/inspect";
+
+export const arcjet = arcjet({
+  key: process.env.ARCJET_KEY,
+  rules: [
+    shield({ mode: "LIVE" }),
+    detectBot({
+      mode: "LIVE",
+      allow: [
+        "CATEGORY:SEARCH_ENGINE", 
+        "CATEGORY:PREVIEW",
+      ],
+    }),
+    tokenBucket({
+      mode: "LIVE",
+      refillRate: 5,
+      interval: 10, 
+      capacity: 20, 
+    }),
+  ],
+});
