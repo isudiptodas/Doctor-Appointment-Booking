a7d88051a6cef0bb1f5d9ddd8f942875dee9bc5c
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2026-01-19 21:17:57 +0530

    Update doctorData.js

--- backend/routes/doctor/doctorData.js
+++ backend/routes/doctor/doctorData.js
@@ -4,7 +4,7 @@ import { Doctor } from '../../models/Doctor.js';
 const router = express.Router();
 
 // fetch all available doctors list
-router.get('/api/doctor-list'), async (req, res) => {
+router.get('/api/all-doctors', async (req, res) => {
    try{
      const found = await Doctor.find();
 
@@ -24,7 +24,7 @@ router.get('/api/doctor-list'), async (req, res) => {
 });
 
 // fetch a single doctor with id
-router.get('/api/doctor'), async (req, res) => {
+router.get('/api/doctor-id', async (req, res) => {
 
   const id = req.query.id;
   
@@ -35,6 +35,36 @@ router.get('/api/doctor'), async (req, res) => {
          return res.status(404).json({
          success: false,
          message: 'Doctor not found',
+       });
+     }
+
+     return res.status(200).json({
+       success: true,
+       message: 'Doctor found',
+       found
+     });
+   }
+   catch(err){
+     console.log(err);
+     return res.status(500).json({
+       success: false,
+       message: 'Something went wrong',
+     });
+   }
+});
+
+// fetch doctors by name
+router.get('/api/doctor-name', async (req, res) => {
+
+  const name = req.query.name
+  
+  try{
+     const found = await Doctor.find({name}).collation({ locale: "en", strength: 2 });
+
+     if(found.length === 0){
+         return res.status(404).json({
+         success: false,
+         message: 'Doctors not found',
        });
      }
 
